6a79fdd602d309f1038fd2e0129ccb9a29250b59
GregorySwart <gregory.swart97@gmail.com>
2020-09-06 00:38:40 +0200

    Added real data and working button controls

--- MOOC_Data_Setup.R
+++ MOOC_Data_Setup.R
@@ -1,7 +1,18 @@
 library(expss)
+library(dplyr)
+library(haven)
 
 ESS9 <- as.data.frame(read_spss("ESS9.sav")) %>% 
-          select(agea, gndr, cntry, eisced, trstprl, trstep, trstun, trstlgl, imsmetn, imdfetn,
-                 imwbcnt, happy, health, frprtpl, grspfr, lrscale)
+          select(agea, gndr, cntry, eisced, dweight,
+                 ppltrst, pplfair, pplhlp, # Individual Trust
+                 trstprl, trstep, trstlgl, # Institutional Trust
+                 imbgeco, imueclt, imwbcnt, # Immigration Perception
+                 impcntr, # Immigration Rejection (SPECIAL)
+                 imsmetn, imdfetn, # Immigration Racial (OPTIONAL)
+                 happy, stflife, # Subjective Satisfaction
+                 frprtpl, stfdem, # Political Satisfaction
+                 stfeco, stfedu, stfhlth, # Institutional Satisfaction
+                 lrscale # Left-Right Scale
+                 )
 
 write_sav(ESS9, "ess_data.sav")
--- app.R
+++ app.R
@@ -5,35 +5,80 @@ library(expss)
 library(dplyr)
 library(gridExtra)
 library(rsconnect)
+library(spatstat)
+library(ggplot2)
+
 
 ##########
 # SERVER #
 ##########
 
-server <- shinyServer(function(input, output) { #generic line initiating the SERVER 
+server <- shinyServer(function(input, output, session) {
   
   #########################
   # Data load and cleanup #
   #########################
   
   #Import data
+  `%nin%` <- Negate(`%in%`)
+  
   ess_data <- as.data.frame(read_spss("ess_data.sav"))
   
-  dummy_data <- data.frame(list("Trust1" = round(runif(5, 0, 10),0),
-                                "Trust2" = round(runif(5, 0, 10),0),
-                                "Trust3" = round(runif(5, 0, 10),0),
-                                "Trust4" = round(runif(5, 0, 10),0),
-                                "Immigration1" = round(runif(5, 0, 10),0),
-                                "Immigration2" = round(runif(5, 0, 10),0),
-                                "Immigration3" = round(runif(5, 0, 10),0),
-                                "Immigration4" = round(runif(5, 0, 10),0),
-                                "Satisfaction1" = round(runif(5, 0, 10),0),
-                                "Satisfaction2" = round(runif(5, 0, 10),0),
-                                "Satisfaction3" = round(runif(5, 0, 10),0),
-                                "Satisfaction4" = round(runif(5, 0, 10),0)),
-                           row.names = c("Min", "Max", "EU_median", "Hungary_median", "Survey_Participant"))
-  dummy_data[1,] <- 0
-  dummy_data[2,] <- 10
+  median_data <- as.data.frame(t(data.frame(rep(0,23),rep(10,23))))
+  
+  colnames(median_data) <- c("agea", "gndr", "eisced",
+    "ppltrst", "pplfair", "pplhlp",
+    "trstprl", "trstep", "trstlgl",
+    "imbgeco", "imueclt", "imwbcnt",
+    "impcntr",
+    "imsmetn", "imdfetn",
+    "happy", "stflife",
+    "frprtpl", "stfdem",
+    "stfeco", "stfedu", "stfhlth",
+    "lrscale")
+  
+  rownames(median_data) <- c("Min","Max")
+  
+  median_data["EU_median",] <- rep(0,23)
+  median_data["HU_median",] <- rep(0,23)
+  median_data["Survey_Participant",] <- runif(23, 2,8) %>% round(0)
+  
+  variables <- c("agea", "gndr", "eisced", "ppltrst", "pplfair", "pplhlp", "trstprl", "trstep", 
+    "trstlgl", "imbgeco", "imueclt", "imwbcnt", "impcntr","imsmetn", "imdfetn","happy", "stflife","frprtpl", 
+    "stfdem","stfeco", "stfedu", "stfhlth", "lrscale")
+  
+  for (i in 1:23){
+    median_data["EU_median",i] <- weighted.median(unlist(ess_data[variables[i]]), ess_data$dweight)
+    median_data["HU_median",i] <- weighted.median(unlist(subset(ess_data, cntry == "HU")[variables[i]]), subset(ess_data, cntry == "HU")$dweight) %>% round(0)
+  }
+  
+  median_data <- select(median_data, -c(agea, gndr, eisced))
+  
+  removed <- c()
+  
+  # {dummy_data <- data.frame(list("ppltrst" = round(runif(5, 0, 10),0),
+  #                               "pplfair" = round(runif(5, 0, 10),0),
+  #                               "pplhlp" = round(runif(5, 0, 10),0),
+  #                               "trstprl" = round(runif(5, 0, 10),0),
+  #                               "trstep" = round(runif(5, 0, 10),0),
+  #                               "trstlgl" = round(runif(5, 0, 10),0),
+  #                               "imbgeco" = round(runif(5, 0, 10),0),
+  #                               "imueclt" = round(runif(5, 0, 10),0),
+  #                               "imwbcnt" = round(runif(5, 0, 10),0),
+  #                               "impcntr" = round(runif(5, 0, 10),0),
+  #                               "imsmetn" = round(runif(5, 0, 10),0),
+  #                               "imdfetn" = round(runif(5, 0, 10),0),
+  #                               "happy" = round(runif(5, 0, 10),0),
+  #                               "stflife" = round(runif(5, 0, 10),0),
+  #                               "frprtpl" = round(runif(5, 0, 10),0),
+  #                               "stfdem" = round(runif(5, 0, 10),0),
+  #                               "stfeco" = round(runif(5, 0, 10),0),
+  #                               "stfedu" = round(runif(5, 0, 10),0),
+  #                               "stfhlth" = round(runif(5, 0, 10),0),
+  #                               "lrscale" = round(runif(5, 0, 10),0)),
+  #                          row.names = c("Min", "Max", "EU_median", "Hungary_median", "Survey_Participant"))
+  # dummy_data[1,] <- 0
+  # dummy_data[2,] <- 10}
   
   #Clean data
   
@@ -41,99 +86,87 @@ server <- shinyServer(function(input, output) { #generic line initiating the SER
   # Reactives #
   #############
   
-  # define any reactive elements of the app
-  
   output$radar_all <- renderPlot({
     colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
     colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
     
-    radarchart(df = dummy_data, 
+    radarchart(df = median_data[which(rownames(median_data) %nin% removed),], 
                cglcol="grey", cglty=1, axislabcol="grey20", axistype = 4, caxislabels = seq(0,10,1), cglwd=1, seg = 10,
-               
-               pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
-               )
-    legend("topright", legend = rownames(dummy_data[c(3,4,5),]), bty = "o", fill=colors_in, cex = 0.9)
+               pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1)
+    
+    legend("topright", legend = rownames(median_data[c(3,4,5),]), bty = "o", fill=colors_in, cex = 0.9)
   })
+
+  #observeEvent({input$redraw_radar})
   
-  output$radar1 <- renderPlot({
-    colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
-    colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
-    
-    radarchart(df = dummy_data %>% select(Trust1,Trust2,Trust3,Trust4), 
-                     cglcol="grey", cglty=1, axislabcol="grey20", caxislabels=seq(0,10,2), cglwd=1,
-                     pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
-                     seg = 5, axistype = 4)
-    #legend("topright", legend = rownames(dummy_data[c(3,4,5),]), bty = "o", fill=colors_in, cex = 1)
-  })
+
+
   
-  output$radar2 <- renderPlot({
-    colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
-    colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
+  observeEvent(input$redraw_radar, {
     
-    radarchart(df = dummy_data %>% select(Immigration1,Immigration2,Immigration3,Immigration4), 
-               cglcol="grey", cglty=1, axislabcol="grey20", caxislabels=seq(0,10,2), cglwd=1,
-               pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
-               seg = 5, axistype = 4)
-    #legend("topright", legend = rownames(dummy_data[c(3,4,5),]), bty = "o", fill=colors_in, cex = 1)
+    if (input$EU_check == T){
+      removed <- removed %>% append("EU_median")
+    }else if(input$EU_check == F){
+      removed <- removed[removed != "EU_median"]
+    }
+    
+    if (input$HU_check == T){
+      removed <- removed %>% append("HU_median")
+    }else if(input$EU_check == F){
+      removed <- removed[removed != "HU_median"]
+    }
+    
+    
+    if (input$own_check == T){
+      removed <- removed %>% append("Survey_Participant")
+    }else if(input$own_check == F){
+      removed <- removed[removed != "Survey_Participant"]
+    }
+    
+    output$radar_all <- renderPlot({
+      colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
+      colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
+      
+      radarchart(df = median_data[which(rownames(median_data) %nin% removed),], 
+                 cglcol="grey", cglty=1, axislabcol="grey20", axistype = 4, caxislabels = seq(0,10,1), cglwd=1, seg = 10,
+                 pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1)
+      
+      legend("topright", legend = rownames(median_data[which(rownames(median_data) %nin% c(removed,"Min","Max")),]), bty = "o", fill=colors_in, cex = 0.9)
+    })
   })
 
-  output$radar3 <- renderPlot({
-    colors_border=c( rgb(0.2,0.5,0.5,0.9), rgb(0.8,0.2,0.5,0.9) , rgb(0.7,0.5,0.1,0.9) )
-    colors_in=c( rgb(0.2,0.5,0.5,0.4), rgb(0.8,0.2,0.5,0.4) , rgb(0.7,0.5,0.1,0.4) )
-    
-    radarchart(df = dummy_data %>% select(Satisfaction1, Satisfaction2,Satisfaction3,Satisfaction4),
-               cglcol="grey", cglty=1, axislabcol="grey20", caxislabels=seq(0,10,2), cglwd=1,
-               pcol=colors_border , pfcol=colors_in , plwd=4 , plty=1,
-               seg = 5, axistype = 4)
-    
-    legend("topright", legend = rownames(dummy_data[c(3,4,5),]), bty = "o", fill=colors_in, cex = 0.9)
-  })
-
-}) #Close the server definition
-
-
-
-
-
+})
 
 ##################
 # User Interface #
 ##################
 
-ui <- shinyUI( #generic line initiating the UI
+ui <- shinyUI(
   
   navbarPage("MOOC App", collapsible = TRUE,
     theme = shinythemes::shinytheme("sandstone"),
     tabPanel("Radar charts",
       fluidRow(
-        column(8, offset = 2, align = "center",
+        column(2, align = "center",
+          checkboxInput("EU_check", label = "Hide EU median"),
+          checkboxInput("HU_check", label = "Hide HU median"),
+          checkboxInput("own_check", label = "Hide own score"),
+          actionButton("redraw_radar", "Redraw plot")
+        ),
+        column(8, align = "center",
           h2("One radar chart"),
-          plotOutput("radar_all", height =800),
+          plotOutput("radar_all", height = 800),
           hr()
-        )
-      ),
-      
-      fluidRow(
-        h2("Separate charts", align = "center"),
-        column(4, align = "center",
-          plotOutput("radar1")
-        ),
-        column(4, align = "center",
-               plotOutput("radar2")
-        ),
-        column(4, align = "center",
-               plotOutput("radar3")
         )
       )
     ),
     tabPanel("Other Data Visualisation")
   )
   
-)  #Close the UI definition
+)
 
 ##############
 # Launch App #
 ##############
 
-#generic line that launches the app
 shinyApp(ui = ui, server = server)
